7ff188471298235e658bc2dc34bf4b5ae79d61ab
Iryna Vyshnevska <iryna.vyshnevska@camptocamp.com>
2022-08-09 10:56:21 +0300

    [IMP] Addaptation to v15

--- pos_lot_selection/__manifest__.py
+++ pos_lot_selection/__manifest__.py
@@ -1,23 +1,25 @@
 # Copyright 2018 Tecnativa S.L. - David Vidal
+# Copyright 2022 Camptocampt
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'POS Lot Selection',
-    'version': '11.0.1.0.0',
-    'category': 'Point of Sale',
-    'author': 'Tecnativa,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/pos',
-    'license': 'AGPL-3',
-    'depends': [
-        'point_of_sale',
+    "name": "POS Lot Selection",
+    "version": "15.0.1.0.0",
+    "category": "Point of Sale",
+    "author": "Tecnativa," "Camptocamp," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/pos",
+    "license": "AGPL-3",
+    "depends": [
+        "web",
+        "point_of_sale",
     ],
-    'data': [
-        'templates/assets.xml',
-    ],
-    'qweb': [
-        'static/src/xml/pos.xml'
-    ],
-    'application': False,
-    'installable': True,
+    "qweb": ["static/src/xml/pos.xml"],
+    'assets': {
+        'point_of_sale.assets': [
+            # 'pos_lot_selection/static/src/js/models.js',
+            # '/pos_lot_selection/static/src/js/chrome.js',
+            'pos_lot_selection/static/src/css/pos.css',
+        ]},
+    "application": False,
+    "installable": True,
 }
